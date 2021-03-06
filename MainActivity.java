1b8232695ab177e6b8d026f539d2db0ebc43a511
NEVELYNE <50985077+NEVELYNE@users.noreply.github.com>
2021-03-06 16:02:17 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.eveswork;
+
+
+
+public class MainActivity {
+
+
+    public static void main(String[] args){
+        System.out.println("CSC3115 Mobile Programming");
+        System.out.println("CSC3116 Artificial Intelligence");
+        System.out.println("CSC3113 Entreprenuership");
+        System.out.println("CSC3104 Computer Networks and Security");
+        System.out.println("CSC3112 Data Communications and Networks");
+        System.out.println("CSC3117 Automata, Complexity and Computability");
+    }
+}
---
+++ MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.eve
+
+fun main(args: Array<String>){
+
+    var Mycourses: Array<String> = arrayOf("CSC3115 MobileProgramming, CSC3 Artificial Intelligence,CSC3113 Entreprenuership, CSC3104 Computer Networks and Security, CSC3112 Data Communications and Networks, CSC3117 Automata Complexity and Computability")
+
+    Mycourses.forEach{System.out.print(it)}
+
+}
