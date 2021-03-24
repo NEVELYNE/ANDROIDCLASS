ea40980a86981b4d2ed36a348e08cd9f26a30331
NEVELYNE <50985077+NEVELYNE@users.noreply.github.com>
2021-03-24 14:51:56 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,44 +1,69 @@
-package must.ac.ug.csce.evelyne.eve004xmlcolorswap;
+package must.ac.ug.csce.evelyne.eve004progcolorswap;
 
 import androidx.annotation.RequiresApi;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.R.color;
 import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import  android.widget.Button;
+import  android.*;
+import android.graphics.Color;
 import java.util.Random;
-
-public class MainActivity extends AppCompatActivity {
-TextView textView;
-Button button;
-float red, blue, green;
-
+import android.widget.RelativeLayout;
+public class MainActivity extends Activity  {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView  textview = (TextView) findViewById(R.id.tV);
-         button = (Button) findViewById(R.id.btn1) ;
+
+
+
+        Button button= new Button(this);
+        //button.setPadding(200,200,200,200);
+        button.setText("Tap Me!");
+
+        TextView textView = new TextView(this);
+       textView.setText("Tap to Change Color");
 
         final Random random= new Random();
-        //button.setVisibility(View.VISIBLE);
-        button.setOnClickListener((v )-> {
-                    float red= random.nextFloat();
 
-                    float green= random.nextFloat();
-                    float blue= random.nextFloat();
-                    textview.setText("r"+String.valueOf(red)+"   g"+String.valueOf(green+" b"+String.valueOf(blue)  ));
-                    textview.setTextColor(Color.rgb(red,green,blue));
-                    //txt.setTextColor(Color);
+        button.setOnClickListener((v )-> {
+            float red= random.nextFloat();
+
+            float green= random.nextFloat();
+            float blue= random.nextFloat();
+            textView.setText("r"+String.valueOf(red)+"   g"+String.valueOf(green+" b"+String.valueOf(blue)  ));
+            textView.setTextColor(Color.rgb(red,green,blue));
+            //txt.setTextColor(Color);
 
         });
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        ll.addView(textView);
+        ll.addView(button);
+        setContentView(ll);
 
     }
-}
+
+
+
+
+
+    }
+
+
+
+
+
